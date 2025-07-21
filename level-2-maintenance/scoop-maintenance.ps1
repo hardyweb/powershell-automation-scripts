634e858c9bbf56f14dcff430f2d473837372ffdf
Hardy N Jafar <nobat80@gmail.com>
2025-07-21 10:46:48 +0800

    Update scoop-maintenance.ps1

--- level-2-maintenance/scoop-maintenance.ps1
+++ level-2-maintenance/scoop-maintenance.ps1
@@ -1,1 +1,13 @@
+ # update-scoop.ps1
+Write-Host "> Updating Scoop itself..." -ForegroundColor Cyan
+scoop update
+
+Write-Host "> Updating all installed Scoop apps..." -ForegroundColor Cyan
+scoop update *
+
+Write-Host "> Cleaning up old versions..." -ForegroundColor Cyan
+scoop cleanup *
+
+Write-Host "> Scoop update and cleanup complete." -ForegroundColor Green
+scoop status
 
